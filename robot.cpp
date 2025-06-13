d349d612b14653b338192b495888cac5318fcf28
KiruthikaSoundararajan <pskiruthika2001@gmail.com>
2025-06-13 18:20:48 +0100

    Made the robot return two steps back if hit the wall.Included conditions to make the user input all lowercase characters

--- robot.cpp
+++ robot.cpp
@@ -1,6 +1,7 @@
 #include "robot.h"
 #include "world.h"
 #include <iostream>
+#include <cctype>
 
 Robot::Robot(int x,int y):x(x),y(y){}
 
@@ -13,28 +14,29 @@ void Robot::move(World& world){
     std::cout <<"Hey!! Where should the robot go?? Tell us using keys!!\n"<< "For up->w, down->s, a->left ,d->right" << "\n"
               << "Or if you feel bored quit using 'q'" << std::endl;
     std::cin >> direction;
+    direction = std::tolower(direction);
 
     switch(direction){
         case 'w':{
             temp=x;
             temp--;
-            if(temp<0) std::cout << "Oh no!! Robot exited the world. Returning to previous position" << std::endl;
-            else x=temp;
+            if(temp<0){ std::cout << "Oh no!! Robot exited the world. Moving two steps backb!!" << std::endl; temp--;}
+            x=temp;
             break;
         }
 
         case 's':{
             temp=x;
             temp++;
-            if(temp>world.getH()) std::cout << "Oh no!! Robot exited the world. Returning to previous position" << std::endl;
-            else x=temp;
+            if(temp>world.getH()){ std::cout << "Oh no!! Robot exited the world. Moving two steps backb!!" << std::endl; temp--;}
+            x=temp;
             break;
         }
 
          case 'a':{
             temp=y;
             temp--;
-            if(temp<0) std::cout << "Oh no!! Robot exited the world. Returning to previous position" << std::endl;
+            if(temp<0){ std::cout << "Oh no!! Robot exited the world. Moving two steps backb!!" << std::endl; temp--;}
             else y=temp;
             break;
         }
@@ -42,7 +44,7 @@ void Robot::move(World& world){
         case 'd':{
             temp=y;
             temp++;
-            if(temp>world.getW()) std::cout << "Oh no!! Robot exited the world. Returning to previous position" << std::endl;
+            if(temp>world.getW()){ std::cout << "Oh no!! Robot exited the world. Moving two steps backb!!" << std::endl; temp--;}
             else y=temp;
             break;
         }
@@ -56,3 +58,5 @@ void Robot::move(World& world){
     std::cout << "Robot's current position: " << x << "," << y << std::endl;
 }
 }
+//case sensitivity
+//also robot goes back to (0,world edge) in the previous position which may not be optimal.so can return back to 2 sreps to move again???
